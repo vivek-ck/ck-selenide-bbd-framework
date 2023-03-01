b29a759c89694fba4c0fca6c416f0efbad42d1e1
vivek-ck <vivek.biswas@cloudkaptan.com>
2023-03-01 13:33:47 +0530

    [CK-00] Added pom.xml and modified .gitignore

---
+++ src/main/java/com/cloudkaptan/qe/App.java
@@ -0,0 +1,13 @@
+package com.cloudkaptan.qe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
