7715313b43c5ae9990daef186341a5ef3ef1dac7
vivek-ck <vivek.biswas@cloudkaptan.com>
2023-06-23 16:55:09 +0530

    Added settlement condition

--- wdio/test/specs/trial.js
+++ wdio/test/specs/trial.js
@@ -53,8 +53,8 @@ import DataStore from '../../utils/datastore.js';
 
         await Home.goTo('Applications')
         await Applications.openApplicationWithId('APP-0000001499');
-        await Applications.addCreditApprovalConditions();
-        // await Applications.addCreditSettlementConditions();
+        // await Applications.addCreditApprovalConditions();
+        await Applications.addCreditSettlementConditions();
 
 
 
