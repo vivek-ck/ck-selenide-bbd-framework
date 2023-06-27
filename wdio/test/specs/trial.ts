092bab0f1d7ff4c0c0049bfdb64bf2030a8ab432
vivek-ck <vivek.biswas@cloudkaptan.com>
2023-06-27 10:03:05 +0530

    Restored the original test script

--- wdio/test/specs/trial.ts
+++ wdio/test/specs/trial.ts
@@ -18,29 +18,30 @@ describe("application_creation_v2", () => {
         await Login.open();
         await Login.login();
 
-        // await Home.goTo('Leads');
+        await Home.goTo('Leads');
 
-        // await Leads.createNewLead({ firstName: firstName, lastName: lastName, email: email });
-        // await Leads.changeLeadStatus('Converted');
-        // await Leads.convertLead();
-        // await Leads.goToCreatedAccount(fullName);
+        await Leads.createNewLead({ firstName: firstName, lastName: lastName, email: email });
+        await Leads.changeLeadStatus('Converted');
+        await Leads.convertLead();
+        await Leads.goToCreatedAccount(fullName);
 
-        // await browser.pause(4000);
-        // await Accounts.editBirthDate('01/01/2000');
-        // await Accounts.goTo('Opportunities');
-        // await Opportunities.openOpportunityWithName(fullName);
-        // await Opportunities.modifyNecessaryDetails();
-        // await Opportunities.createAndOpenApplication();
+        await browser.pause(4000);
+        await Accounts.editBirthDate('01/01/2000');
+        await Accounts.goTo('Opportunities');
+        await Opportunities.openOpportunityWithName(fullName);
+        await Opportunities.modifyNecessaryDetails();
+        await Opportunities.createAndOpenApplication();
 
-        await Home.goTo('Applications');
-        let applicationId = 'APP-0000001543';
-        // let applicationId = await DataStore.getData('applicationId');
-        await Applications.openApplicationWithId(applicationId);
-        // await Applications.editLoan();
-        // await Applications.addNewCollateral(fullName);
-        // await Applications.addParties(fullName);
-        // await Applications.addCreditApprovalConditions();
-        // await Applications.addCreditSettlementConditions();
+        // await Home.goTo('Applications');
+        // let applicationId = 'APP-0000001543';
+        // // let applicationId = await DataStore.getData('applicationId');
+        // await Applications.openApplicationWithId(applicationId);
+        
+        await Applications.editLoan();
+        await Applications.addNewCollateral(fullName);
+        await Applications.addParties(fullName);
+        await Applications.addCreditApprovalConditions();
+        await Applications.addCreditSettlementConditions();
         await Applications.addTaskList();
         console.log('100');
     });
