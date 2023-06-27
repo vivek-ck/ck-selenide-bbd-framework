efe299988bd1cac647e18eae42a7e62616f54907
puja-ck <puja.karmakar@cloudkaptan.com>
2023-06-27 14:29:44 +0530

    credit approval

--- wdio/test/specs/trial.ts
+++ wdio/test/specs/trial.ts
@@ -8,43 +8,43 @@ import Opportunities from '../../pages/opportunities';
 import Applications from '../../pages/applications';
 
 describe("application_creation_v2", () => {
-    it("tests application_creation_v2", async () => {
+//     it("tests application_creation_v2", async () => {
 
-        const firstName = faker.person.firstName()
-        const lastName = faker.person.lastName()
-        const email = faker.internet.email()
-        const fullName = `${firstName} ${lastName}`;
-        await Login.open()
-        await Login.loginAsCreditOfficer()
+//         const firstName = faker.person.firstName()
+//         const lastName = faker.person.lastName()
+//         const email = faker.internet.email()
+//         const fullName = `${firstName} ${lastName}`;
+//         await Login.open()
+//         await Login.loginAsCreditOfficer()
 
-        await Home.goTo('Leads')
+//         await Home.goTo('Leads')
 
-        await Leads.createNewLead({ firstName: firstName, lastName: lastName, email: email })
-        await Leads.changeLeadStatus('Converted')
-        await Leads.convertLead()
-        await Leads.goToCreatedAccount(fullName)
+//         await Leads.createNewLead({ firstName: firstName, lastName: lastName, email: email })
+//         await Leads.changeLeadStatus('Converted')
+//         await Leads.convertLead()
+//         await Leads.goToCreatedAccount(fullName)
 
-        await browser.pause(4000)
-        await Accounts.editBirthDate('01/01/2000')
-        await Accounts.goTo('Opportunities')
-        await Opportunities.openOpportunityWithName(fullName)
-        await Opportunities.modifyNecessaryDetails()
-        await Opportunities.createAndOpenApplication()
+//         await browser.pause(4000)
+//         await Accounts.editBirthDate('01/01/2000')
+//         await Accounts.goTo('Opportunities')
+//         await Opportunities.openOpportunityWithName(fullName)
+//         await Opportunities.modifyNecessaryDetails()
+//         await Opportunities.createAndOpenApplication()
 
-        // await Home.goTo('Applications')
-        // let applicationId = 'APP-0000001543';
-        // // let applicationId = await DataStore.getData('applicationId')
-        // await Applications.openApplicationWithId(applicationId)
+//         // await Home.goTo('Applications')
+//         // let applicationId = 'APP-0000001543';
+//         // // let applicationId = await DataStore.getData('applicationId')
+//         // await Applications.openApplicationWithId(applicationId)
 
-        await Applications.editLoan()
-        await Applications.addNewCollateral(fullName)
-        await Applications.addParties(fullName)
-        await Applications.addCreditApprovalConditions()
-        await Applications.addCreditSettlementConditions()
-        await Applications.addTaskList()
-        console.log('100')
-    })
-})
+//         await Applications.editLoan()
+//         await Applications.addNewCollateral(fullName)
+//         await Applications.addParties(fullName)
+//         await Applications.addCreditApprovalConditions()
+//         await Applications.addCreditSettlementConditions()
+//         await Applications.addTaskList()
+//         console.log('100')
+//     })
+// })
 it("tests application_creation_v2", async () => {
     await Login.open()
     //await Login.loginAsRaUser()
@@ -71,5 +71,5 @@ it("tests application_creation_v2", async () => {
     console.log('mshdgvfs')
 
 })
-
+})
 //APP-0000001499
