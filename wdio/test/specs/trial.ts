d05c544947aeea5c04e98762cfc1478fa6297544
vivek-ck <vivek.biswas@cloudkaptan.com>
2023-07-05 16:34:07 +0530

    Fixed the issue of skipping tests inside foreach

--- wdio/test/specs/trial.ts
+++ wdio/test/specs/trial.ts
@@ -1,22 +1,25 @@
-import { faker } from '@faker-js/faker';
-import path from 'path';
+import { faker } from '@faker-js/faker'
 
-import Home from "../../pages/home";
-import Leads from "../../pages/leads";
-import Login from "../../pages/login";
-import Accounts from '../../pages/accounts';
-import Opportunities from '../../pages/opportunities';
-import Applications from '../../pages/applications';
-import readXlsxFile from 'read-excel-file/node'
-import schema from '../../utils/excelSchema/customerData.json'
-import excelDataProvider from '../../utils/excelDataProvider';
-import customerData from '../../utils/excelSchema/customerData';
+import Home from "../../pages/home"
+import Leads from "../../pages/leads"
+import Login from "../../pages/login"
+import Accounts from '../../pages/accounts'
+import Opportunities from '../../pages/opportunities'
+import Applications from '../../pages/applications'
+import customerDataSchema from '../../utils/excelSchema/customerData.json'
+import customerDataType from '../../utils/excelSchema/customerData'
+import excelDataProvider from '../../utils/excelDataProvider'
+
 
 describe("application_creation_v2", async () => {
-    const data = (await excelDataProvider('automation.xlsx', schema))
-        .rows
-        .map((row: any) => row as customerData)
-    data.forEach(({
+    const excelData = excelDataProvider('automation.xlsx', customerDataSchema).map((row: any) => row as customerDataType)
+
+    before('Login', async () => {
+        await Login.open();
+        await Login.login();
+    })
+
+    excelData.forEach(({
         scenarioNumber,
         product,
         guarantor,
@@ -33,32 +36,33 @@ describe("application_creation_v2", async () => {
         settlementConditions,
         fees,
         creditComments,
-        opsComments
-    }: customerData) => {
+        opsComments,
+    }) => {
+
         it(`Creates valid data for entry ${scenarioNumber}`, async () => {
             const firstName = faker.person.firstName();
             const lastName = faker.person.lastName();
             const email = faker.internet.email();
             const fullName = `${firstName} ${lastName}`;
+
             await Login.open();
-            await Login.login();
-            // await Home.goTo('Leads');
+            await Home.goTo('Leads');
 
-            // await Leads.createNewLead({ firstName: firstName, lastName: lastName, email: email });
-            // await Leads.changeLeadStatus('Converted');
-            // await Leads.convertLead();
-            // await Leads.goToCreatedAccount(fullName);
+            await Leads.createNewLead({ firstName: firstName, lastName: lastName, email: email });
+            await Leads.changeLeadStatus('Converted');
+            await Leads.convertLead();
+            await Leads.goToCreatedAccount(fullName);
 
-            // await browser.pause(4000);
-            // await Accounts.editBirthDate('01/01/2000');
-            // await Accounts.goTo('Opportunities');
-            // await Opportunities.openOpportunityWithName(fullName);
-            // await Opportunities.modifyNecessaryDetails(product, amount, termMonths, loanUse);
-            // await Opportunities.createAndOpenApplication();
+            await browser.pause(4000);
+            await Accounts.editBirthDate('01/01/2000');
+            await Accounts.goTo('Opportunities');
+            await Opportunities.openOpportunityWithName(fullName);
+            await Opportunities.modifyNecessaryDetails(product, amount, termMonths, loanUse);
+            await Opportunities.createAndOpenApplication();
 
-            // await Applications.editLoan();
-            // await Applications.addNewCollateral(fullName);
-            // await Applications.addParties(fullName);
+            await Applications.editLoan();
+            await Applications.addNewCollateral(fullName);
+            await Applications.addParties(fullName);
         });
     });
 
--- wdio/utils/excelDataProvider.ts
+++ wdio/utils/excelDataProvider.ts
@@ -1,7 +1,37 @@
-import readXlsxFile from 'read-excel-file/node'
+import xlsx from 'node-xlsx';
 import path from 'path';
 
-export default async function excelDataProvider(filename: string, excelSchema: any) {
+export default function excelDataProvider(filename: string, schema: object) {
     let absFilePath = path.resolve('samplefiles/', filename)
-    return await readXlsxFile(absFilePath, { map: excelSchema });
+    let rowData = xlsx.parse(absFilePath)[0].data
+    let allColumns = rowData[0]
+    let validColumns = getValidColumnsWithIndex(allColumns, schema)
+    let parsedData: (typeof schema) [] = []
+
+    for (let j = 1; j < rowData.length; ++j) {
+        let columnData = {}
+        let undefinedCount = 0
+        for (let key in validColumns) {
+            let cellValue = rowData[j][validColumns[key]]
+            if (typeof cellValue === 'undefined') { 
+                undefinedCount++
+                cellValue = ""
+            }
+            columnData[key] = cellValue
+        }
+        if(undefinedCount !== Object.keys(validColumns).length)
+        parsedData.push(columnData)
+    }
+
+    return parsedData
 }
+
+function getValidColumnsWithIndex(columns: string[], schema: object) {
+    let columnsWithIndex = {}
+    for (let i = 0; i < columns.length; ++i) {
+        if (schema.hasOwnProperty(columns[i])) {
+            columnsWithIndex[schema[columns[i]]] = i;
+        }
+    }
+    return columnsWithIndex
+} 
